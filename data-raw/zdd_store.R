35a950fffc8ca01de13f0263699697730efd9ee2
Jordan <Jordan@karens-MacBook-Air.local>
2020-11-29 09:53:02 -0800

    Update zdd_store.R

--- data-raw/zdd_store.R
+++ data-raw/zdd_store.R
@@ -1,3 +1,5 @@
 ## code to prepare `zdd_store` dataset goes here
-
+# when package loads, we want to have a reliable environment where we can store
+# zdd nodes
+zdd_store <- new.env()
 usethis::use_data(zdd_store, overwrite = TRUE)
