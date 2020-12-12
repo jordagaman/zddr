1725a21a8303193e524bcc2c2bad56617f477a0d
Jordan <65474982+jordagaman@users.noreply.github.com>
2020-12-12 15:19:56 -0800

    add cutset printing capability

---
+++ R/cutsets.R
@@ -0,0 +1,29 @@
+#' cutsets
+#'
+#' @param zdd a zdd list object
+#'
+#' @return a list of integer vectors representing a family of sets (cutsets)
+#' @export
+#'
+#' @examples cutsets( zdd_union(as_zdd(3L), as_zdd(4L) ) )
+#' @examples cutsets( zdd_union(as_zdd(4L), as_zdd(3L) ) )
+#' @examples cutsets( zdd_union(as_zdd(4L), zdd0()     ) )
+#' @examples cutsets( zdd_union(as_zdd(4L), zdd1()     ) )
+cutsets <- function(zdd) {
+  if( is_one(zdd)) return(list( c() ))
+  if(is_zero(zdd)) return(list(     ))
+  add_to_every_vector <- function(list_of_vectors, value) {
+    purrr::map(
+      list_of_vectors,
+      function(x) c(x, value)
+    )
+  }
+  p0_cutsets <- cutsets(get_zdd(zdd$p0))
+  p1_cutsets <- cutsets(get_zdd(zdd$p1))
+  return(
+    append(
+      p0_cutsets,
+      add_to_every_vector(p1_cutsets, zdd$value)
+    )
+  )
+}
