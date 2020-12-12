47831253a076a59bc47f57dc551f3f69f96f4846
Jordan <Jordan@karens-MacBook-Air.local>
2020-12-12 08:50:42 -0800

    clean up tests

--- tests/testthat/test-zdd.R
+++ tests/testthat/test-zdd.R
@@ -1,8 +1,6 @@
 test_that("hashes still work", {
-  # we only know this because we ran this earlier. if it ever changes, something is probably wrong.
   p0 <- '420767073edd8b7097448d6c27bf5534'
-  p1 <- '420767073edd8b7097448d6c27bf5534'
-  node <- zdd(1L, p0, p1)
+  node <- zdd(1L, p0, p0)
   expect_equal(node$p0, p0)
-  expect_equal(node$p1, p1)
+  expect_equal(node$p1, p0)
 })
