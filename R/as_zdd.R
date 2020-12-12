8367c3d8e9d4165711ff31c86ae2252097c840c9
Jordan <Jordan@karens-MacBook-Air.local>
2020-12-12 09:53:22 -0800

    add as_zdd

---
+++ R/as_zdd.R
@@ -0,0 +1,15 @@
+#' as_zdd
+#'
+#' @param value the integer value of the node to be added
+#'
+#' @return the single variable elementary zdd (the so-called "set containing `value`")
+#' @export
+#'
+#' @examples as_zdd(3L)
+as_zdd <- function(value) {
+  zdd(
+    value = value,
+    p0    = zdd0()$hash,
+    p1    = zdd1()$hash
+  )
+}
