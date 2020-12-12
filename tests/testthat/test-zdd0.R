15486465ad6a4d64906afbda6fbb5ca531c998b1
Jordan <65474982+jordagaman@users.noreply.github.com>
2020-12-12 10:58:54 -0800

    reset environment to allow these tests to work

--- tests/testthat/test-zdd0.R
+++ tests/testthat/test-zdd0.R
@@ -1,9 +1,13 @@
 test_that("zdd0 works", {
   # zdd0 should add d93e93351850a7687ef0cadda5f46681 to zdd_store
+  remove(
+    list = ls(envir = zdd_store),
+    envir = zdd_store
+  )
   test_func <- function() {
     exists('d93e93351850a7687ef0cadda5f46681', envir = zddr::zdd_store)
   }
-  #expect_false(test_func())  # this isn't working. suspect that the environment from one test bleeding into this one.
+  expect_false(test_func())
   zdd0()
   expect_true(test_func())
 })
--- tests/testthat/test-zdd1.R
+++ tests/testthat/test-zdd1.R
@@ -1,9 +1,13 @@
 test_that("zdd1 works", {
   # zdd1 should add fca1155cd0feba8b69b4a1f015941e04 to zdd_store
+  remove(
+    list = ls(envir = zdd_store),
+    envir = zdd_store
+  )
   test_func <- function() {
     exists('fca1155cd0feba8b69b4a1f015941e04', envir = zddr::zdd_store)
   }
-  #expect_false(test_func())  # this isn't working. suspect that the environment from one test bleeding into this one.
+  expect_false(test_func())
   zdd1()
   expect_true(test_func())
 })
