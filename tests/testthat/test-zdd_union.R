b1e5c0d7adc74f40fc91d43447f5a53a6c980380
Jordan <65474982+jordagaman@users.noreply.github.com>
2020-12-13 10:17:49 -0800

    hwitespace

--- tests/testthat/test-zdd_union.R
+++ tests/testthat/test-zdd_union.R
@@ -1,7 +1,7 @@
 test_that("zdd_union works", {
   expec   <- list( c(3L) , c(4L) )
-  result1 <- cutsets( zdd_union(as_zdd(4L), as_zdd(3L) ) )
-  result2 <- cutsets( zdd_union(as_zdd(3L), as_zdd(4L) ) )
+  result1 <- cutsets( zdd_union(as_zdd(4L) , as_zdd(3L) ) )
+  result2 <- cutsets( zdd_union(as_zdd(3L) , as_zdd(4L) ) )
   result3 <- cutsets(           as_zdd(3L) | as_zdd(4L)  )
   expect_equal(expec, result1)
   expect_equal(expec, result2)
