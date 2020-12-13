1c59ed21681a6a4db02e5a247f69b9cb7854cc4c
Jordan <65474982+jordagaman@users.noreply.github.com>
2020-12-13 09:40:36 -0800

    update documentation

--- R/as_zdd.R
+++ R/as_zdd.R
@@ -1,29 +1,29 @@
 #' as_zdd
 #'
-#' @param value the integer value of the node to be added
+#' @param x the integer value of the node to be added or the string hash of the node to pull from the store
 #'
 #' @return the single variable elementary zdd (the so-called "set containing `value`")
 #' @export
 #'
 #' @examples as_zdd(3L)
 #' as_zdd('aeca5759d5d7a2fca7544cd6baba7bd5')
-#' as_zdd('this one does not exist')
+#' #as_zdd('this one does not exist')  # this one throws an error
 as_zdd <- function(x) {
   UseMethod("as_zdd")
 }
 
 #' @export
-as_zdd.integer <- function(value) {
+as_zdd.integer <- function(x) {
   zdd(
-    value = value,
+    value = x,
     p0    = zdd0()$hash,
     p1    = zdd1()$hash
   )
 }
 
 #' @export
-as_zdd.character <- function(hash) {
-  if(!exists(hash, envir = zddr::zdd_store))
+as_zdd.character <- function(x) {
+  if(!exists(x, envir = zddr::zdd_store))
     stop('hash not found in zdd_store')
-  get(hash, envir = zddr::zdd_store)
+  get(x, envir = zddr::zdd_store)
 }
