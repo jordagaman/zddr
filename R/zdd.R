ce96a838dc8e542c1a6215fbf65911799dc8f66e
Jordan <65474982+jordagaman@users.noreply.github.com>
2020-12-13 10:28:38 -0800

    allow the zdd constructor to accept p0 and p1 as a zdd node or a hash string

--- R/zdd.R
+++ R/zdd.R
@@ -1,8 +1,8 @@
 #' zdd
 #'
 #' @param value the integer value of the node
-#' @param p0 the negative compliment of the zdd with respect to `value`, expressed as class zdd
-#' @param p1 the positive compliment of the zdd with respect to `value`, expressed as class zdd
+#' @param p0 the negative compliment of the zdd with respect to `value`, expressed as a string hash or as class zdd
+#' @param p1 the positive compliment of the zdd with respect to `value`, expressed as a string hash or as class zdd
 #'
 #' @return the resulting zdd node, expressed as class zdd
 #' @export
@@ -10,11 +10,13 @@
 #' @examples
 #' zdd(1L, '420767073edd8b7097448d6c27bf5534', '420767073edd8b7097448d6c27bf5534')
 zdd <- function(value, p0, p1) {
-  hash <- zdd_hash(value, p0, p1)
+  P0 <- ifelse(is_zdd(p0), p0$hash, p0)
+  P1 <- ifelse(is_zdd(p1), p1$hash, p1)
+  hash <- zdd_hash(value, P0, P1)
   if(!exists(hash, envir = zddr::zdd_store))
     assign(
       x     = hash,
-      value = list(value = value, p0 = p0, p1 = p1, hash = hash),
+      value = list(value = value, p0 = P0, p1 = P1, hash = hash),
       envir = zddr::zdd_store
     )
   res <- as_zdd(hash)
