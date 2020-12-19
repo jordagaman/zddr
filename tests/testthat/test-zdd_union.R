03ca3e4b0286175de68f5e38b6c6f1dac50c9816
Jordan <65474982+jordagaman@users.noreply.github.com>
2020-12-19 13:23:31 -0800

    add some sweet tests

--- tests/testthat/test-zdd_union.R
+++ tests/testthat/test-zdd_union.R
@@ -6,4 +6,28 @@ test_that("zdd_union works", {
   expect_equal(expec, result1)
   expect_equal(expec, result2)
   expect_equal(expec, result3)
+
+  library(magrittr)
+  zdd345 <-
+    zdd(3L, zdd0(), zdd1()) %>%
+    zdd(4L, zdd0(), .     ) %>%
+    zdd(5L, zdd0(), .     )
+  zdd35  <-
+    zdd(3L, zdd0(), zdd1()) %>%
+    zdd(5L, zdd0(), .     )
+  zdd45  <-
+    zdd(4L, zdd0(), zdd1()) %>%
+    zdd(5L, zdd0(), .     )
+  zdd34  <-
+    zdd(3L, zdd0(), zdd1()) %>%
+    zdd(4L, zdd0(), .     )
+
+  cutsets(zdd345)         %>% expect_equal( list(          c(3L,4L,5L)) )
+  cutsets(zdd35)          %>% expect_equal( list(          c(3L,   5L)) )
+  cutsets(zdd45)          %>% expect_equal( list(          c(   4L,5L)) )
+  cutsets(zdd34)          %>% expect_equal( list(          c(3L,4L   )) )
+  cutsets(zdd345 | zdd35) %>% expect_equal( list(c(3L,5L), c(3L,4L,5L)) )
+  cutsets(zdd345 | zdd45) %>% expect_equal( list(          c(   4L,5L)) )
+  #cutsets(zdd345 | zdd34)   %>% expect_equal( list(          c(3L,4L   )) )
+
 })
