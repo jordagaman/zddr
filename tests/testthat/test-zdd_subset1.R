d5c53dc84a6bc8c4841652409fde5567401fad63
Jordan <65474982+jordagaman@users.noreply.github.com>
2020-12-21 12:09:56 -0800

    clean up tests

--- tests/testthat/test-zdd_subset1.R
+++ tests/testthat/test-zdd_subset1.R
@@ -1,36 +1,55 @@
 test_that("subset1 works", {
   expect_true(is_one(zdd_subset1(as_zdd(2L), 2L)))# ONE
-  expect_true(zdd_subset1(as_zdd(2L), 1L) == as_zdd(2L))# {2}
   expect_true(is_zero(zdd_subset1(as_zdd(1L), 2L)))# ZERO
   library(magrittr)
-  zdd345 <-
-    zdd(3L, zdd0(), zdd1()) %>%
-    zdd(4L, zdd0(), .     ) %>%
+  zdd24 <-
+    zdd(2L, zdd0(), zdd1()) %>%
+    zdd(4L, zdd0(), .     )
+  zdd135 <-
+    zdd(1L, zdd0(), zdd1()) %>%
+    zdd(3L, zdd0(), .     ) %>%
     zdd(5L, zdd0(), .     )
+
+  ####
+  # some basic tests to start
+  ####
   expect_equal(
-    cutsets(zdd_subset1(zdd345, 4L)),
-    list(c(3L,5L))
+    cutsets(zdd_subset1(zdd135, 5L)),     list(c(1L,3L))
   )
   expect_equal(
-    cutsets(zdd_subset1(zdd345, 5L)),
-    list(c(3L,4L))
+    cutsets(zdd_subset1(zdd135, 4L)),     list()
   )
   expect_equal(
-    cutsets(zdd_subset1(zdd345, 3L)),
-    list(c(4L,5L))
+    cutsets(zdd_subset1(zdd135, 3L)),     list(c(1L,5L))
+  )
+
+  ####
+  # some tests on a more complex zdd
+  ####
+  expect_equal(
+    cutsets(zdd_subset1(zdd135 | zdd24, 5L)),     list(c(1L,3L))
+  )
+  expect_equal(
+    cutsets(zdd_subset1(zdd135 | zdd24, 4L)),     list(c(2L))
+  )
+  expect_equal(
+    cutsets(zdd_subset1(zdd135 | zdd24, 3L)),     list(c(1L,5L))
+  )
+  expect_equal(
+    cutsets(zdd_subset1(zdd135 | zdd24, 2L)),     list(c(4L))
+  )
+  expect_equal(
+    cutsets(zdd_subset1(zdd135 | zdd24, 1L)),     list(c(3L,5L))
   )
 
   expect_equal(
-    cutsets(zdd_subset1(zdd345 | as_zdd(6L), 5L)),
-    list(c(3L,4L),c(6))
+    cutsets(zdd_subset1(zdd135 | as_zdd(6L), 6L)),     list(NULL)
   )
   expect_equal(
-    cutsets(zdd_subset1(zdd345 | as_zdd(6L), 6L)),
-    list(NULL)
+    cutsets(zdd_subset1(zdd135 | as_zdd(6L), 7L)),     list()
   )
   expect_equal(
-    cutsets(zdd_subset1(zdd345 | as_zdd(6L), 7L)),
-    list()
+    cutsets(zdd_subset1(zdd135 | as_zdd(6L) | as_zdd(7L), 7L)), list(NULL)
   )
 
 
