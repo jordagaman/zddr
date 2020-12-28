612064e84485b86c88c6b051981091b37d011a58
Jordan <65474982+jordagaman@users.noreply.github.com>
2020-12-28 15:20:45 -0800

    add one last super greedy test

--- data-raw/benchmarking.R
+++ data-raw/benchmarking.R
@@ -227,3 +227,23 @@ Sys.time() - start_time            # Time difference of 43.60355 secs
 pryr::object_size(zddr::zdd_store) # 1.07 MB
 pryr::object_size(zddr::zdd_fxns)  # 6.18 MB
 
+
+
+######### COMPARISON - GREEDY ZDD MULTIPLICATION with a 10 nonminimal additions
+library(zddr)
+reset_zdd_store()
+start_time <- Sys.time()
+zddr:::zdd_count(
+  (zdd_or(   1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20) *
+     zdd_or(21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40) *
+     zdd_or(41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60) *
+     zdd_or(61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80) *
+     zdd_or(81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100) *
+     zdd_or(101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120)) |
+    zdd_and(41,81) | zdd_and(28,88) | zdd_and(29, 107) | zdd_and(21,101) | zdd_and(23,83)
+) # [1] 63,200,805
+
+######### PERFORMANCE STATS
+Sys.time() - start_time            # Time difference of 16.01262 mins
+pryr::object_size(zddr::zdd_store) # 18.8 MB
+pryr::object_size(zddr::zdd_fxns)  # 109 MB
