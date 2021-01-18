720df440cf1b6ad031c6ea79033885499bab5949
Jordan <65474982+jordagaman@users.noreply.github.com>
2021-01-18 10:44:20 -0800

    update as_zdd documentation

--- R/as_zdd.R
+++ R/as_zdd.R
@@ -1,35 +1,49 @@
-#' as_zdd
+#' Coerce an integer or string hash to a ZDD object
 #'
-#' @param x the integer value of the node to be added or the string hash of the node to pull from the store
+#' @description
+#' `r lifecycle::badge("experimental")`
 #'
-#' @return the single variable elementary zdd (the so-called "set containing `value`")
+#' `as_zdd()` turns an existing object, such as a numeric/integer value or a
+#' string that corresponds to a previously created ZDD, into a registered ZDD
+#' object of class `zdd`.
+#'
+#' @param x the integer value of the node to be added or the string hash of the
+#'     node to pull from the store
+#'
+#' @return the single variable elementary zdd (the so-called "set containing
+#'    `value`")
 #' @export
 #'
 #' @examples
 #' as_zdd(3L)
 #' as_zdd('d7281766b28bdc9025520edeeffecdd9')
 #' #as_zdd('this one does not exist')  # this one throws an error
+#'
 as_zdd <- function(x) {
   UseMethod("as_zdd")
 }
 
 #' @export
+#' @rdname as_zdd
 as_zdd.zdd <- function(x) {
   return(x)
 }
 
 #' @export
+#' @rdname as_zdd
 as_zdd.integer <- function(x) {
   return(zdd(x))
 }
 
 #' @export
+#' @rdname as_zdd
 as_zdd.numeric <- function(x) {
   stopifnot(all.equal(x, as.integer(x)))
   return(zdd(x))
 }
 
 #' @export
+#' @rdname as_zdd
 as_zdd.character <- function(x) {
   if(!zdd_exists(x)) stop('hash not found in zdd_store')
   return(zddr::zdd_store[[x]])
