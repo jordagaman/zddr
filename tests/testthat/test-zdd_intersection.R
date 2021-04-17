074d5243325ba781e03c38a06c51ea30526b3459
Jordan <65474982+jordagaman@users.noreply.github.com>
2021-04-16 18:29:06 -0700

    fixes #17

--- tests/testthat/test-zdd_intersection.R
+++ tests/testthat/test-zdd_intersection.R
@@ -7,5 +7,8 @@ test_that("intersection works", {
 
   ( (zdd345 | zdd35) & (zdd34 | zdd35) ) %>% expect_equal( zdd35  )
   ( (zdd345 | zdd(1)) &  zdd345        ) %>% expect_equal( zdd345 )
+  is_zero( zdd35 & (zdd345 | zdd34)    ) %>% expect_true()
   is_zero( (zdd345 | zdd34) &  zdd35   ) %>% expect_true()
+  is_zero(   zdd345 & T                ) %>% expect_true()
+  is_zero(   T & zdd345                ) %>% expect_true()
 })
